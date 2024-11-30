f02e39d0d877d81136e2d937d8dd0163c772e073
Yesung Cho <143062013+yesung05@users.noreply.github.com>
2024-11-30 16:52:34 +0900

    Update Test1.c

--- 기말/Project1/Test1.c
+++ 기말/Project1/Test1.c
@@ -6,15 +6,13 @@ int main() {
 	int beyul[4] = { 1 };
 
 	for (int i = 0; i < 4; i++) {
-		printf("¹è¿­ÀÇ[%d] °ª %d\n", i, beyul[i]);
+		printf("Â¹Ã¨Â¿Â­Ã€Ã‡[%d] Â°Âª %d\n", i, beyul[i]);
 	}
 
 	return 0;
 }
 
 int plus(int a) {
-
-	//static int b = a;
 
 	return 0;
 }
